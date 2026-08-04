16fb26109b1ad57ff5e8a81cd17bc11dbc7ba2f7
Amresh Maurya <amresh.terminal@gmail.com>
2026-08-04 23:05:09 +0530

    add datbase and security config

---
+++ src/main/java/amreshmaurya/com/payflow/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package amreshmaurya.com.payflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.FilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+
+        return httpSecurity.formLogin(AbstractHttpConfigurer::disable).formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+
+}
