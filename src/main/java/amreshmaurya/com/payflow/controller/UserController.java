7680215fbe4e9159c18ddb4e787075d8c6096f8c
Amresh Maurya <amresh.terminal@gmail.com>
2026-08-09 19:52:35 +0530

    complete user

--- src/main/java/amreshmaurya/com/payflow/controller/UserController.java
+++ src/main/java/amreshmaurya/com/payflow/controller/UserController.java
@@ -1,33 +1,90 @@
 package amreshmaurya.com.payflow.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import amreshmaurya.com.payflow.api.ApiResponse;
+import amreshmaurya.com.payflow.dto.user.CreateUser;
+import amreshmaurya.com.payflow.dto.user.UpdateUser;
+import amreshmaurya.com.payflow.dto.user.UserResponse;
+import amreshmaurya.com.payflow.service.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import amreshmaurya.com.payflow.dto.user.CreateUser;
-import amreshmaurya.com.payflow.dto.user.UserResponse;
-import amreshmaurya.com.payflow.service.UserService;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
-    
+
     private final UserService userService;
 
+    // CREATE
     @PostMapping
-    public ResponseEntity<ApiResponse<UserResponse>> createUser(@Valid @RequestBody CreateUser createUser){
-        return  ResponseEntity.status(HttpStatus.CREATED).body( ApiResponse.<UserResponse>builder().data(userService.createUser(createUser)).success(true).message("User created successfully!").build());
+    public ResponseEntity<ApiResponse<UserResponse>> createUser(
+            @Valid @RequestBody CreateUser createUser) {
+
+        UserResponse userResponse = userService.createUser(createUser);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        ApiResponse.<UserResponse>builder()
+                                .data(userResponse)
+                                .success(true)
+                                .message("User created successfully!")
+                                .build()
+                );
     }
 
+    // GET BY ID
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<UserResponse>> getUserById(@Valid @PathVariable UUID id){
-        return  ResponseEntity.ok(ApiResponse.<UserResponse>builder().data(userService.getUserById(id)).success(true).message("User Found Successfully").build());
+    public ResponseEntity<ApiResponse<UserResponse>> getUserById(
+            @PathVariable UUID id) {
+
+        UserResponse userResponse = userService.getUserById(id);
+
+        return ResponseEntity.ok(
+                ApiResponse.<UserResponse>builder()
+                        .data(userResponse)
+                        .success(true)
+                        .message("User found successfully!")
+                        .build()
+        );
     }
 
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<UserResponse>> updateUser(
+            @PathVariable UUID id,
+            @Valid @RequestBody UpdateUser updateUser) {
+
+        UserResponse userResponse = userService.updateUser(id, updateUser);
+
+        return ResponseEntity.ok(
+                ApiResponse.<UserResponse>builder()
+                        .data(userResponse)
+                        .success(true)
+                        .message("User updated successfully!")
+                        .build()
+        );
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteUser(
+            @PathVariable UUID id) {
+
+        userService.deleteUser(id);
+
+        return ResponseEntity.ok(
+                ApiResponse.<Void>builder()
+                        .data(null)
+                        .success(true)
+                        .message("User deleted successfully!")
+                        .build()
+        );
+    }
 }
--- src/main/java/amreshmaurya/com/payflow/service/UserService.java
+++ src/main/java/amreshmaurya/com/payflow/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import amreshmaurya.com.payflow.dto.user.CreateUser;
+import amreshmaurya.com.payflow.dto.user.UpdateUser;
 import amreshmaurya.com.payflow.dto.user.UserResponse;
 import amreshmaurya.com.payflow.entity.User;
 import amreshmaurya.com.payflow.repository.UserRepository;
@@ -45,6 +46,54 @@ public class UserService {
 
         return UserResponse.builder().id(user.getId()).name(user.getName()).email(user.getEmail())
                 .phone(user.getPhone()).build();
+    }
+
+
+
+    public UserResponse updateUser(UUID id, UpdateUser updateUser) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found with id: " + id)
+                );
+
+        if (updateUser.getEmail() != null) {
+            user.setEmail(updateUser.getEmail());
+        }
+
+        if (updateUser.getPhone() != null) {
+            user.setPhone(updateUser.getPhone());
+        }
+
+        if (updateUser.getName() != null) {
+            user.setName(updateUser.getName());
+        }
+
+        if (updateUser.getPassword() != null) {
+            user.setPassword(
+                    passwordEncoder.encode(updateUser.getPassword())
+            );
+        }
+
+        User savedUser = userRepository.save(user);
+
+        return UserResponse.builder()
+                .id(savedUser.getId())
+                .name(savedUser.getName())
+                .email(savedUser.getEmail())
+                .phone(savedUser.getPhone())
+                .build();
+    }
+
+
+    public void deleteUser(UUID id) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found with id: " + id)
+                );
+
+        userRepository.delete(user);
     }
 
 }
